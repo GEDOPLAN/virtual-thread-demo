57d4578bb52f771f2317895e8d84e84276f2ccdb
Dirk Weil <dirk.weil@gedoplan.de>
2023-03-19 18:52:12 +0100

    Code aufgeräumt

--- src/test/java/de/gedoplan/showcase/ThreadDemo.java
+++ src/test/java/de/gedoplan/showcase/ThreadDemo.java
@@ -12,12 +12,33 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadDemo {
 
+  private long start = System.currentTimeMillis();
   private AtomicInteger threadCount = new AtomicInteger();
   private Phaser barrier = new Phaser(2);
   private Logger logger = Logger.getLogger(ThreadDemo.class);
 
+  /**
+   * Start threads by constructing them.
+   * This is not recommended - use a thread pool instead.
+   */
   @Test
-  void classic() {
+  void thread() {
+    // while (true) {
+    for (int i = 0; i < 10_000; ++i) {
+      // Start doSomething in new (platform) thread
+      new Thread(this::doSomething).start();
+
+      // Wait for new thread to be alive before continuing with the next one
+      barrier.arriveAndAwaitAdvance();
+    }
+  }
+
+  /**
+   * Start threads from pool.
+   * In this special case, no thread is reused, because they run indefinitely.
+   */
+  @Test
+  void pool() {
     ExecutorService executor = Executors.newCachedThreadPool();
     // while (true) {
     for (int i = 0; i < 10_000; ++i) {
@@ -29,9 +50,14 @@ public class ThreadDemo {
     }
   }
 
+  /**
+   * Start threads from virtual thread executor.
+   * No thread is reused, because they are so cheap.
+   */
   @Test
   void virtual() {
-    final ThreadFactory factory = Thread.ofVirtual().name("virtual-", 1).factory();
+
+    ThreadFactory factory = Thread.ofVirtual().name("virtual-", 1).factory();
     ExecutorService executor = Executors.newThreadPerTaskExecutor(factory);
     // while (true) {
     for (int i = 0; i < 1_000_000; ++i) {
@@ -48,7 +74,9 @@ public class ThreadDemo {
     // Increment and print thread count (every 1000)
     int count = threadCount.incrementAndGet();
     if ((count % 1000) == 0) {
-      logger.infof("%,9d @ %s", count, Thread.currentThread());
+      long elapsedMillis = System.currentTimeMillis() - start;
+
+      logger.infof("%,9d (%d ns/thread) %s", count, elapsedMillis*1000/count, Thread.currentThread());
     }
 
     // Tell main, I'm alive
